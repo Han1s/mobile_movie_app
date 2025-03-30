2ac903814945efcfbe9c13f73c3f3ed6cf5c2783
Jan Ranostaj <HonzaRanostaj@gmail.com>
2025-03-30 19:07:05 +0200

    2h19m30s add metrics db update function

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "expo-router";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useEffect, useState } from "react";
+import { updateSearchCount } from "@/services/appwrite";
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -30,6 +31,10 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        if (movies?.length > 0 && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies?.[0]);
+        }
       } else {
         reset();
       }
